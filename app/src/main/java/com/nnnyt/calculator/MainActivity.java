9aa630a7e0275fa14efb886f193d16310fc42940
NNNYT <975845193@qq.com>
2017-02-12 21:38:38 +0800

    ~

--- app/src/main/java/com/nnnyt/calculator/MainActivity.java
+++ app/src/main/java/com/nnnyt/calculator/MainActivity.java
@@ -32,25 +32,25 @@ public class MainActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn0= (Button) findViewById(R.id.btn_0);
-        btn1= (Button) findViewById(R.id.btn_1);
-        btn2= (Button) findViewById(R.id.btn_2);
-        btn3= (Button) findViewById(R.id.btn_3);
-        btn4= (Button) findViewById(R.id.btn_4);
-        btn5= (Button) findViewById(R.id.btn_5);
-        btn6= (Button) findViewById(R.id.btn_6);
-        btn7= (Button) findViewById(R.id.btn_7);
-        btn8= (Button) findViewById(R.id.btn_8);
-        btn9= (Button) findViewById(R.id.btn_9);
-        btnPoint= (Button) findViewById(R.id.btn_point);
-        btnAdd= (Button) findViewById(R.id.btn_add);
-        btnMinus= (Button) findViewById(R.id.btn_minus);
-        btnMultiply= (Button) findViewById(R.id.btn_multiply);
-        btnDivide= (Button) findViewById(R.id.btn_divide);
-        btnC= (Button) findViewById(R.id.btn_c);
-        btnDel=(Button)findViewById(R.id.btn_del);
-        btnEqual= (Button) findViewById(R.id.btn_equal);
-        etInput= (EditText) findViewById(R.id.et_input);
+        btn0 = (Button) findViewById(R.id.btn_0);
+        btn1 = (Button) findViewById(R.id.btn_1);
+        btn2 = (Button) findViewById(R.id.btn_2);
+        btn3 = (Button) findViewById(R.id.btn_3);
+        btn4 = (Button) findViewById(R.id.btn_4);
+        btn5 = (Button) findViewById(R.id.btn_5);
+        btn6 = (Button) findViewById(R.id.btn_6);
+        btn7 = (Button) findViewById(R.id.btn_7);
+        btn8 = (Button) findViewById(R.id.btn_8);
+        btn9 = (Button) findViewById(R.id.btn_9);
+        btnPoint = (Button) findViewById(R.id.btn_point);
+        btnAdd = (Button) findViewById(R.id.btn_add);
+        btnMinus = (Button) findViewById(R.id.btn_minus);
+        btnMultiply = (Button) findViewById(R.id.btn_multiply);
+        btnDivide = (Button) findViewById(R.id.btn_divide);
+        btnC = (Button) findViewById(R.id.btn_c);
+        btnDel = (Button) findViewById(R.id.btn_del);
+        btnEqual = (Button) findViewById(R.id.btn_equal);
+        etInput = (EditText) findViewById(R.id.et_input);
         btn0.setOnClickListener(this);
         btn1.setOnClickListener(this);
         btn2.setOnClickListener(this);
@@ -71,32 +71,69 @@ public class MainActivity extends Activity implements View.OnClickListener {
         btnEqual.setOnClickListener(this);
 
 
-
     }
 
     @Override
     public void onClick(View v) {
-        String str=etInput.getText().toString();
+        String str = etInput.getText().toString();
         switch (v.getId()) {
-            case R.id.btn_0:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_1:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_2:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_3:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_4:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_5:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_6:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_7:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_8:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_9:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_point:etInput.setText(str+((Button)v).getText());break;
-            case R.id.btn_c:etInput.setText("");break;
-            case R.id.btn_del:if(str!=null&&!str.equals("")){
-                etInput.setText(str.substring(0,str.length()-1));
+            case R.id.btn_0:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_1:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_2:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_3:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_4:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_5:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_6:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_7:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_8:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_9:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_point:
+                etInput.setText(str + ((Button) v).getText());
+                break;
+            case R.id.btn_c: {
+                str = "";
+                etInput.setText(str);
             }
-            case R.id.btn_add:etInput.setText(str+" "+((Button)v).getText()+" ");
-            case R.id.btn_minus:etInput.setText(str+" "+((Button)v).getText()+" ");
-            case R.id.btn_multiply:etInput.setText(str+" "+((Button)v).getText()+" ");
-            case R.id.btn_divide:etInput.setText(str+" "+((Button)v).getText()+" ");
+            break;
+            case R.id.btn_del:
+                if (str != null && !str.equals("")) {
+                    str = str.substring(0, str.length() - 1);
+                    etInput.setText(str);
+                }
+                ;
+                break;
+            case R.id.btn_add:
+                etInput.setText(str + " " + ((Button) v).getText() + " ");
+                break;
+            case R.id.btn_minus:
+                etInput.setText(str + " " + ((Button) v).getText() + " ");
+                break;
+            case R.id.btn_multiply:
+                etInput.setText(str + " " + ((Button) v).getText() + " ");
+                break;
+            case R.id.btn_divide:
+                etInput.setText(str + " " + ((Button) v).getText() + " ");
+                break;
 
         }
     }
